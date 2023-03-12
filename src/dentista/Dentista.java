fb9bc09a4bf261dda9aa008ca05b065af3fb7a73
pedro <pedro@DESKTOP-RENS9N9>
2023-03-11 23:54:09 -0600

    Primer commit

---
+++ src/dentista/Dentista.java
@@ -0,0 +1,16 @@
+
+package dentista;
+
+/**
+ * Universidad Politécnica de San Luis Potosí
+ * @author 174074
+ * Pedro Antonio Saldívar Juárez 174074
+ * Programación III Grupo 7-8
+ */
+public class Dentista {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
